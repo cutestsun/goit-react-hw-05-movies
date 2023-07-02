158a8b6aed1a0bbdb87e896bc1e87f9fe0b0319d
cutestsun <118314867+cutestsun@users.noreply.github.com>
2023-07-02 14:42:06 +0000

    Deploying to gh-pages from @ cutestsun/goit-react-hw-05-movies@57520f2fc8347e4a1ed143f99f45734e68bf360a 🚀

---
+++ static/js/805.550c1020.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,n,r){r.r(n),r.d(n,{default:function(){return s}});var t=r(87),i=r(184);function s(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)("h1",{children:"Page not found"}),(0,i.jsxs)("p",{children:["Please, go to main page:"," ",(0,i.jsx)(t.rU,{to:"/",children:(0,i.jsx)("b",{children:"Link"})})]})]})}}}]);
+//# sourceMappingURL=805.550c1020.chunk.js.map
